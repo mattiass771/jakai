9a19cfab93e8cc5388bab0cc0d433c27d448b1b4
Matus Zilinsky <matus.zilinsky@accenture.com>
2021-03-21 12:58:30 +0100

    switch lightbox to controlled carousel

--- client/src/components/Blocks/LightBox.js
+++ client/src/components/Blocks/LightBox.js
@@ -2,6 +2,7 @@ import React, { useEffect, useState } from 'react'
 
 import Modal from 'react-bootstrap/Modal'
 import Image from 'react-bootstrap/Image'
+import Carousel from 'react-bootstrap/Carousel'
 
 import {ImCancelCircle} from 'react-icons/im'
 import {MdNavigateNext, MdNavigateBefore} from 'react-icons/md'
@@ -13,22 +14,36 @@ export default ({expand, setExpand, getImage, images}) => {
         setActualImage(images.findIndex(val => val === expand))
     }, [expand])
 
-    const handleNextImage = () => {
-        const nextIndex = actualImage < images.length-1 ? actualImage+1 : 0
-        setActualImage(nextIndex)
-    }
+    const handleSelect = (selectedIndex, e) => {
+        setActualImage(selectedIndex);
+    };
 
-    const handlePrevImage = () => {
-        const prevIndex = actualImage > 0 ? actualImage-1 : images.length-1
-        setActualImage(prevIndex)
+    const showImages = () => {
+        return images.map((image, i) => {
+            return (
+                <Carousel.Item style={{width: '100%', background: 'rgba(0,0,0,0.0)'}}>
+                    <Image style={{width: '100%'}} src={getImage(images[i])} rounded fluid />
+                </Carousel.Item>
+            )
+        })
     }
 
     return (
-        <Modal style={{background: 'rgba(0,0,0,0)'}} size="lg" show={typeof expand === 'string' && expand.length !== 0} onHide={() => setExpand('')}>
-            <Image src={getImage(images[actualImage])} rounded fluid />
-            <MdNavigateNext className="link-no-deco" style={{ position: 'absolute', right: -15, top: '45%', fontSize: "500%"}} onClick={() => handleNextImage()} />
-            <MdNavigateBefore className="link-no-deco"  style={{  position: 'absolute', left: -15, top: '45%', fontSize: "500%"}} onClick={() => handlePrevImage()} />
-            <ImCancelCircle className="link-no-deco" style={{position: 'absolute', right: -15, top: -15, fontSize: "200%"}} onClick={() => setExpand('')} />
+        <Modal style={{background: 'rgba(0,0,0,0.0)'}} size="lg" show={typeof expand === 'string' && expand.length !== 0} onHide={() => setExpand('')}>
+            <Carousel 
+                nextIcon={<MdNavigateNext style={{fontSize: '500%', color: '#333333'}} />} 
+                prevIcon={<MdNavigateBefore style={{fontSize: '500%', color: '#333333'}} />} 
+                style={{width: '100%', background: 'rgba(0,0,0,0.0)'}} 
+                fade={true}
+                onSelect={handleSelect} 
+                activeIndex={actualImage} 
+                indicators={false} 
+                interval={null} 
+                fluid
+            >
+                {showImages()}
+            </Carousel>
+            <ImCancelCircle className="link-no-deco" style={{zIndex: '+5', position: 'absolute', right: -15, top: -15, fontSize: "200%"}} onClick={() => setExpand('')} />
         </Modal>
     )
 }
